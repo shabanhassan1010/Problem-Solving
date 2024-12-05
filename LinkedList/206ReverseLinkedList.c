d888bda781c07c96968850f259f6f0207b4c742b
Shaban Hassan <shabanhassan1010@gmail.com>
2024-12-05 02:28:47 +0200

    Solved 206. Reverse Linked List

---
+++ LinkedList/206ReverseLinkedList.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int value;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) 
+{
+    struct ListNode *prev = NULL;      // Tracks the previous node
+    struct ListNode *temp = head;     // Tracks the current node
+    struct ListNode *next = NULL;
+    while (temp != NULL) {
+        next = temp->next;  
+        temp->next = prev;  // Reverse the next pointer
+        prev = temp;        
+        temp = next;        
+    } 
+   return prev; // New head of the reversed list   
+}
