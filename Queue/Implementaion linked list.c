06b6444d18c4c6e8d9231b414459dbecedfd247c
Shaban Hassan <shabanhassan1010@gmail.com>
2024-12-22 15:55:10 +0200

    Implementaion linked list

---
+++ Queue/Implementaion linked list.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+struct queue
+{
+    int front;
+    int rear;
+    int date;
+};
+
+void Enqueue(int date)
+{
+
+}
+
+void Dequeue()
+{
+    
+}
+
+int main()
+{
+    printf("Hello World");
+    return 0;
+}
