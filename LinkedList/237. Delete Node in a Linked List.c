5b6b7a65d1d182302ea061c9ddbea42861e483b6
Shaban Hassan <shabanhassan1010@gmail.com>
2024-12-23 03:12:46 +0200

    Added some changes

--- LinkedList/237. Delete Node in a Linked List.c
+++ LinkedList/237. Delete Node in a Linked List.c
@@ -6,17 +6,17 @@
     struct ListNode *next;
  };
 
- void deleteNode(struct ListNode* node) 
+ void deleteNode(struct ListNode* node)      // he give me the node >>>> 5
 {
-    if(node == NULL || node->next  == NULL)
+    if(node == NULL || node->next  == NULL)  // at least must exist two nodes
     {
     return;
     }
     else
     {
-        struct ListNode *temp = node->next;
-        node->val = temp->val;
-        node->next = temp->next;
+        struct ListNode *temp = node->next;   // Hna el-temp hykon b3d node
+        node->val = temp->val;                // take a copy from the temp  >> node = 1
+        node->next = temp->next;             // hna el-node el next bet3ha bysawar 3la el-next 1>> bysawar 3la 9
         free(temp);
     } 
 }
