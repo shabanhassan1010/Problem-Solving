9713bc59dc2415348f3d5f17e39f659847248a78
Shaban Hassan <shabanhassan1010@gmail.com>
2024-12-29 18:10:22 +0200

    Added some changes in merge sort

--- Algorithms/Margging Two List.c
+++ Algorithms/Margging Two List.c
@@ -4,36 +4,37 @@
 // Merging Two List in single list....
 void MarggingTwoList(int arr1[] , int arr2[] , int arr3[] , int sizeForArr1 , int sizeForArr2)
 {
-        int i = 0, j = 0, k = 0;
+    int i = 0, j = 0, k = 0;
     while(i < sizeForArr1 && j < sizeForArr2)
     {
         if(arr1[i] < arr2[j])
         {
             arr3[k] = arr1[i];
             i++;
-            k++;
         }
         else
         {
-            arr3[k] = arr1[j];
+            arr3[k] = arr2[j];
             j++;
-            k++;
         }
+            k++;
     }
+
     while(i < sizeForArr1)
     {
         arr3[k] = arr1[i];
         i++;
         k++;
     }
+
     while(j < sizeForArr2)
     {
-        arr3[k] = arr1[j];
+        arr3[k] = arr2[j];
         j++;
         k++;
     }
-}
 
+}
 
 // Merging Two List in single list....
 void Mergging(int arr1[] , int left , int mid , int right)
@@ -43,6 +44,7 @@ void Mergging(int arr1[] , int left , int mid , int right)
     int k = left;
     int j = mid + 1;
     int arr2 [20];
+
     while(i <= mid && j <= right)
     {
         if(arr1[i] < arr1[j])
@@ -59,28 +61,34 @@ void Mergging(int arr1[] , int left , int mid , int right)
         arr2[k] = arr1[i];
         k++;
     }
+
     while(j <= left )
     {
         arr2[k] = arr1[j];
         k++;
     }
+
     for(int i = 0 ; i <= right ; i++)
     {
         arr1[i] = arr2[i];
     }
+
 }
 
 int main()
 {
-    int arr[] = {1 , 5 , 7 ,2 ,10 , 9};
-    int n1 = 6;
-    int arr2[] = {4 , 10 , 6 ,2 ,11 , 9};
-    int n2 = 6;
+    int arr1[] = {1, 3, 5, 7 , 10 , 100};
+    int n1 = sizeof(arr1) / sizeof(arr1[0]);
+
+    int arr2[] = {2, 4, 6, 8};
+    int n2 = sizeof(arr2) / sizeof(arr2[0]);
+
     int arr3[n1 + n2];
-    MarggingTwoList(arr , arr2 , arr3 , n1 , n2 );
+    MarggingTwoList(arr1 , arr2 , arr3 , n1 , n2 );
+    
     for(int i = 0 ; i < n1 + n2 ; i++)
     {
-        printf("%d" , arr3[i]);
+        printf("%d " , arr3[i]);
             printf("\n");
     }
 
