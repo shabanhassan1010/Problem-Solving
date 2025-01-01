447f8375b8caa7052ff6751f52c8a3d67e81f22f
Shaban Hassan <shabanhassan1010@gmail.com>
2025-01-01 21:31:12 +0200

    875. Koko Eating Bananas

---
+++ Binary Search/875. Koko Eating Bananas.cs
@@ -0,0 +1,26 @@
+﻿public class Solution 
+{
+    public int MinEatingSpeed(int[] piles, int h) 
+    {
+        int Left = 1;  // I start eating from 1 beacause its not valid to start from zero.
+        int right = piles.Max();
+        if(h < piles.Length)     return -1;  // must be hours bigger then or equal piles[i]
+        while(Left <= right)    // beacause i will started from Left
+        {
+            int mid = (Left + right) / 2;
+            int totalHours = 0;
+            int res = 0;
+            for(int i = 0 ; i < piles.Length ; i++)
+            {
+                totalHours += (piles[i] + mid - 1) / mid; //   >>> 5 / 2 = 3 not 2
+            }
+            if(totalHours == h )  
+                return mid;
+            if(totalHours <= h )  
+                right = mid - 1;    
+            else
+                Left = mid + 1; 
+        }
+        return Left;
+    }
+}
