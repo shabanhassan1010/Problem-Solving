71ace98aba7939b75e98e40c3517b2cbc34f0ce9
Shaban Hassan <shabanhassan1010@gmail.com>
2025-01-03 03:10:14 +0200

    added some changes

--- Array/2899. Last Visited Integers.cs
+++ Array/2899. Last Visited Integers.cs
@@ -8,23 +8,22 @@
 
         foreach (int num in nums) // test case: [1,2,-1,-1,-1]
         {
-            if (num != -1) // 1 != -1 so added | 2 != -1 so added  | -1 != -1 false | -1 != -1 false  | -1 != -1 false 
+            if (num != -1)       // 1 != -1 so added | 2 != -1 so added  | -1 != -1 false | -1 != -1 false  | -1 != -1 false 
             {
-                seen.Add(num);  // seen[1]    | seen[1 , 2]        |
+                seen.Add(num);  // seen[1]           | seen[1 , 2]       |
                 k = 0;         
             }
             else
             {
-                k++;   // 1   |    2    |    3
+                k++;                  //         1         |         2      |         3
                 if (k <= seen.Count)  // if (1 <= 2) true  |  (2 <= 2) true |  if (3 <= 2) false 
                     ans.Add(seen[seen.Count - k]); 
                     // ans.add(seen(2-1))= 2  => ans.add(seenOfIndex[1]) = 2  =>     ans[2]  
-                    // ans.add(seen(2-2))= 0  => ans.add(seenOfIndex[1]) = 2  =>     ans[2 , 1]  |   
+                    // ans.add(seen(2-2))= 0  => ans.add(seenOfIndex[1]) = 2  =>     ans[2 , 1]    
                 else
                     ans.Add(-1);  //  ans[ 2 , 1 , -1]
             }
         }
-
         return ans;
     }
 }
