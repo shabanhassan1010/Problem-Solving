36945584a6b8753a3ef5c366d44a0ee92d2d5598
Shaban Hassan <shabanhassan1010@gmail.com>
2025-01-29 18:08:05 +0200

    solve 58. Length of Last Word

---
+++ String/58. Length of Last Word.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Solution {
+    public int LengthOfLastWord(string s) 
+    {
+        int count = 0 ;
+        int theEndOfString = s.Length-1;
+        while(theEndOfString >= 0 && s[theEndOfString] == ' ')
+        theEndOfString --;
+        while(theEndOfString >= 0 && s[theEndOfString] != ' ')
+        {
+            count ++;
+            theEndOfString--;  // if i do not write this line it will be infinite loop 
+        }
+        return count;
+    }
+}
