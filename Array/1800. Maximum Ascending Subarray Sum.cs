57fe2f68b9e61a084e933aff01eff3d235c18e17
Shaban Hassan <shabanhassan1010@gmail.com>
2025-02-07 02:26:32 +0200

    solve 1800. Maximum Ascending Subarray Sum

---
+++ Array/1800. Maximum Ascending Subarray Sum.cs
@@ -0,0 +1,20 @@
+﻿public class Solution 
+{
+    public int MaxAscendingSum(int[] nums)
+    {
+
+        int MaxSize = nums[0];
+        int CurrrentNumber = nums[0];
+        for(int i = 1; i < nums.Length; i++)
+        {
+                if (nums[i] > nums[i - 1])
+                    CurrrentNumber += nums[i];
+                else
+                    CurrrentNumber = nums[i];
+                if (CurrrentNumber > MaxSize)
+                    MaxSize = CurrrentNumber;
+        }
+            
+            return MaxSize;
+    }
+} 
