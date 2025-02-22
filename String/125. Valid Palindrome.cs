b6c53c782a703f92cede570e42126a6b6f56b69d
Shaban Hassan <shabanhassan1010@gmail.com>
2025-02-22 05:34:31 +0200

    125. Valid Palindrome

---
+++ String/125. Valid Palindrome.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+public class Solution {
+    public bool IsPalindrome(string s) 
+    {
+        string cleaned = Regex.Replace(s, "[^a-zA-Z0-9]", "").ToLower();
+        for(int i = 0 ; i < cleaned.Length ; i++)
+        {
+            if (cleaned[i] != cleaned[cleaned.Length - 1 - i]) // i use i so do not reapted steos more than one
+            return false;
+        }
+        return true;
+    }
+}
