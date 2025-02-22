7bf39106594edae35b52920a0541c188245bd1e5
Shaban Hassan <shabanhassan1010@gmail.com>
2025-02-22 05:03:07 +0200

    125. Valid Palindrome

---
+++ Two Pointers/392. Is Subsequence.cs
@@ -0,0 +1,17 @@
+﻿public class Solution {
+    public bool IsSubsequence(string s, string t)
+    {
+        int i = 0;
+        int j = 0;
+        while(i < s.Length && j < t.Length)
+        {
+            if(s[i] == t[j])
+            {
+              i++;  
+            }
+            j++;
+        }
+        // Return true if all characters in s were found in t, false otherwise.
+        return i == s.Length;  // s.Length -> المقصود بيها هل الحروف اللي في الكلمه التانبه طلعت مساويه للحروف في الكلمه الاولي ولا لا 
+    }
+}
