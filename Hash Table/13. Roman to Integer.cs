d1b6e0843a52830c59e954a2a7d53c4aa47bb2d4
Shaban Hassan <shabanhassan1010@gmail.com>
2025-02-23 19:33:50 +0200

    13. Roman to Integer

---
+++ Hash Table/13. Roman to Integer.cs
@@ -0,0 +1,19 @@
+﻿public class Solution {
+    public int RomanToInt(string s) 
+    {
+        Dictionary<char, int> romanMap = new Dictionary<char, int>() {
+        {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},{'C', 100}, {'D', 500}, {'M', 1000}};
+        int total  = 0 ;
+        for(int i = 0 ; i < s.Length -1 ; i++)
+        {
+            int currectIndex = romanMap[s[i]];
+            int NextIndex  = romanMap[s[i+1]];
+            if(currectIndex >= NextIndex)
+            total += currectIndex;
+            else
+            total -= currectIndex;
+        }   
+        total += romanMap[s[s.Length-1]]; 
+        return total;
+    }
+}
