95e2b56f14056e3fd7835ccb25faa16faa51d26b
Shaban Hassan <shabanhassan1010@gmail.com>
2025-03-09 06:00:51 +0200

    1528. Shuffle String

---
+++ String/1528. Shuffle String.cs
@@ -0,0 +1,12 @@
+﻿public class Solution {
+    public string RestoreString(string s, int[] indices) 
+    {
+        // deivide s , indices into 2
+        char [] res = new char[s.Length];
+        for(int i = 0 ; i < s.Length ; i++)
+        {
+            res[indices[i]] = s[i];
+        } 
+        return new string(res);  
+    }
+}
