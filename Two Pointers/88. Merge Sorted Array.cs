f84cdc0919d9149fcafc9e34234e04add196d4ca
Shaban Hassan <shabanhassan1010@gmail.com>
2025-03-17 04:48:52 +0200

    88. Merge Sorted Array

---
+++ Two Pointers/88. Merge Sorted Array.cs
@@ -0,0 +1,23 @@
+﻿public class Solution {
+    public void Merge(int[] nums1, int m, int[] nums2, int n) 
+    {
+        int i = 0 , j = 0 , k = 0;
+        int [] NewArr = new int[ m + n];
+        while (i < m && j < n)
+        {
+            if(nums1[i] < nums2[j])
+            NewArr[k++] = nums1[i++];
+            else
+            NewArr[k++] = nums2[j++];
+        }
+        while(i < m)
+        {
+            NewArr[k++] = nums1[i++];
+        }
+        while(j < n)
+        {
+            NewArr[k++] = nums2[j++];
+        }
+            Array.Copy(NewArr, nums1, NewArr.Length);
+    }
+}
